49d0df2d4723010ce3cf082d9c990f5c1fc3bf5a
Andres Torres <andres.torres.marroquin@gmail.com>
2011-12-19 00:14:21 -0600

    First stable version

---
+++ django_dropbox_project/dropbox_testing/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django_dropbox.storage import DropboxStorage
+
+STORAGE = DropboxStorage()
+
+class Person(models.Model):
+     photo = models.ImageField(upload_to='photos', storage=STORAGE, null=True, blank=True)
+     resume = models.FileField(upload_to='resumes', storage=STORAGE, null=True, blank=True)
