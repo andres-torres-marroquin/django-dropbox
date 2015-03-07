085b305a3068b3f1ac52280945d717b7ea075ecf
Andy Dai <daikeren@gmail.com>
2015-03-07 16:50:34 +0800

    Updated settings. Get ACCESS_TYPE  from Django settings.

--- django_dropbox/settings.py
+++ django_dropbox/settings.py
@@ -7,5 +7,4 @@ ACCESS_TOKEN_SECRET = getattr(settings, 'DROPBOX_ACCESS_TOKEN_SECRET', None)
 CACHE_TIMEOUT = getattr(settings, 'DROPBOX_CACHE_TIMEOUT', 3600 * 24 * 365)   # One year
 
 # ACCESS_TYPE should be 'dropbox' or 'app_folder' as configured for your app
-ACCESS_TYPE = 'dropbox'
-
+ACCESS_TYPE = getattr(settings, 'ACCESS_TYPE', 'dropbox')
