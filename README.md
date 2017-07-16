1c602e58d6abfa4af329bf4e64d541ec5e0c17d6
maxicecilia <maxicecilia@gmail.com>
2017-07-16 11:38:04 -0300

    Bump dropbox to 8.0.0 in setup.py

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # django-dropbox
-> Version 0.1.1
+> Version 0.1.2
 
 # What
 
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def get_packages():
 
     return packages
 
-requires = ['dropbox==7.3.1']
+requires = ['dropbox==8.0.0']
 
 setup(name='django-dropbox',
       version=version,
